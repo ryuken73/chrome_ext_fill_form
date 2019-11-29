3f0b5785580b9a45e20fab577968be4e88ebfaf6
ryuken <ryuken73@gmail.com>
2019-11-29 14:05:45 +0900

    set default api url

--- options.js
+++ options.js
@@ -1,17 +1,43 @@
 const urlInput = document.getElementById('sbssrUrl');
 const submitBtn = document.getElementById('submit');
+const clearBtn = document.getElementById('clear');
+const defaultURL = 'http://10.10.1.14:3002/infra/sbssr/api';
 
+const savedKey = 'sbssrUrl';
 const restore_options = () => {
-    const savedKey = 'sbssrUrl';
     chrome.storage.local.get(savedKey, (result) => {
-        urlInput.value = result ? result[savedKey] : 'http://10.10.1.14:3002/infra/sbssr/api';
+        console.log(result);
+        console.log(result[savedKey]);
+        if(result[savedKey] == undefined){
+            // set default value
+            console.log('result empty!');
+            chrome.storage.local.set({sbssrUrl:defaultURL}, () => {
+                console.log(`set default value : ${url}`);
+                urlInput.value = defaultURL;
+            });
+            return;
+        }
+        console.log('result not empty!')
+        urlInput.value = result[savedKey];
     })
 }
 
 submitBtn.addEventListener('click', (e) => {
     const url = urlInput.value;
-    alert(url);
+    alert(`saved! ${url}`);
     chrome.storage.local.set({sbssrUrl:url}, () => console.log(`set local storage : ${url}`));
+})
+
+clearBtn.addEventListener('click', (e) => {
+    alert(`clear value!`);
+    try {
+        chrome.storage.local.remove(savedKey, (result) => {
+            console.log('delete callback!')
+            console.log(result);
+        })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 document.addEventListener('DOMContentLoaded', restore_options);
